00e2b22f6a007b740724eb40b94369da14ed86eb
batiskaaaf <batiskaf1@outlook.com>
2023-06-21 13:58:47 +0200

    Add clients and scopes to config

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -17,10 +17,18 @@ namespace IdentityServer
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
-            { };
+            { new ApiScope("webapi", "WebApi") };
 
         public static IEnumerable<Client> Clients =>
             new Client[] 
-            { };
+            {   
+                new Client()
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedScopes = {"webapi"}
+                } 
+            };
     }
 }
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -20,9 +20,6 @@ namespace IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // uncomment, if you want to add an MVC-based UI
-            //services.AddControllersWithViews();
-
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
@@ -30,10 +27,8 @@ namespace IdentityServer
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients);
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
+                .AddInMemoryClients(Config.Clients)
+                .AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app)
