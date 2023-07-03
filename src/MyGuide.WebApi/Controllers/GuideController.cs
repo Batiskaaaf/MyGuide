1ac625ac572e3cafd72a677bafc8ee262d2c0d4d
batiskaaaf <batiskaf1@outlook.com>
2023-07-03 21:02:21 +0200

    Add getusersguidesasync endpoint

--- src/MyGuide.WebApi/Controllers/GuideController.cs
+++ src/MyGuide.WebApi/Controllers/GuideController.cs
@@ -1,25 +1,42 @@
+
+
 namespace MyGuide.WebApi.Controllers;
 
 /// <summary>
 /// Controller for managing user guids
 /// </summary>
-[Route("{userId}/[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
+[Authorize]
 public class GuidesController : ControllerBase
 {
+    private readonly IGuideService guideService;
+    private readonly IMapper mapper;
+
     /// <summary>
     /// Ctor with dependencies
     /// </summary>
-    public GuidesController()
+    public GuidesController(
+        IGuideService guideService,
+        IMapper mapper
+    )
     {
+        this.guideService = guideService;
+        this.mapper = mapper;
     }
 
     /// <summary>
-    /// Description
+    /// Gets all user guides
     /// </summary>
+    /// <param name="userId">User identifier</param>
+    /// <returns>List of users guides</returns>
     [HttpGet]
-    public ActionResult<string> GetGuidesAsync()
+    [AllowAnonymous]
+    [Route("~/api/v1/{userId}/[controller]")]
+    public async Task<ActionResult<IEnumerable<GuideShortResponce>>> GetUsersGuidesAsync(Guid userId)
     {
-        return Ok("Hello world");
+        var guidesDto = await guideService.GetUsersGuidesAsync(userId);
+        var guidesShortResponce = mapper.Map<IEnumerable<GuideShortResponce>>(guidesDto);
+        return Ok(guidesShortResponce);
     }
 }
